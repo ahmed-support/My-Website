1fbd020ea12182ef9d3681ec60ca1b02a8bb57da
Ahmed S. Abdi <ugaaska320@gmail.com>
2025-06-15 19:44:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+const base = process.env.NODE_ENV === 'production' ? "/ahmed-abdi-portfolio/" : "/";
+
 export default defineConfig(({ mode }) => ({
+  base,
   server: {
     host: "::",
     port: 8080,
