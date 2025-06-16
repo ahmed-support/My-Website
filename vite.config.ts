35e57dd65bd0192221e7be5a74be70b05ab1f2bd
Ahmed S. Abdi <ugaaska320@gmail.com>
2025-06-16 21:22:21 +0300

    set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const base = process.env.NODE_ENV === 'production' ? "/ahmed-abdi-portfolio/" : "/";
+base: mode === 'production' ? '/ahmed-abdi-portfolio/' : '/',
 
 export default defineConfig(({ mode }) => ({
   base,
