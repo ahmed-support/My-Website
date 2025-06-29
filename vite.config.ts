f165ea0db9acbc6f6936fbc3b3de9b38e19c271b
Ahmed S. Abdi <ugaaska320@gmail.com>
2025-06-29 09:29:26 +0300

    Fix base path for custom domain - vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/ahmed-abdi-portfolio/' : '/',
+  base: mode === 'production' ? './' : '/',
+
   server: {
     host: "::",
     port: 8080,
